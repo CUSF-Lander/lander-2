c2fc38e22ece6fe5eb88a535b924132b8be8cf9e
Krystian Filipek <kf446@cam.ac.uk>
2026-02-07 17:02:37 +0000

    Improved ESP-NOW communication and data robustness
    - Fixed channel for ESP-NOW
    - Add global spinlock to protect shared sensor data access between IMU callback and ESP-NOW sender
    - Converted flow control variables to std::atomic to fix race conditions and 64 bit reads

--- main/espnow_init.hpp
+++ main/espnow_init.hpp
@@ -1,6 +1,33 @@
 #ifndef ESPNOW_INIT_HPP
 #define ESPNOW_INIT_HPP
 
+#include <cstdint>
+
+//ESP-NOW data structure for sensor telemetry
+typedef struct __attribute__((packed)) {
+    uint32_t sequence;
+    uint32_t timestamp;
+    
+    //Euler Angles
+    float roll;
+    float pitch;
+    float yaw;
+    
+    //Linear Acceleration
+    float lin_acc_x;
+    float lin_acc_y;
+    float lin_acc_z;
+    
+    //Position
+    float pos_x;
+    float pos_y;
+    float pos_z;
+    
+    //Environment
+    float pressure;
+
+} esp_now_data_t;
+
 void init_espnow_sender();
 void esp_now_send_data();
 
